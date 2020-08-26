f5014b8b63de3bd9186eb4f84a5a082d590423c2
masa <masatoshi.sboard@gmail.com>
2020-08-27 04:55:41 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -4,5 +4,14 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        a: usize,
+        b: usize,
+        c: usize,
+        d: usize,
+        e: usize,
     }
+    let mut array = [a, b, c, d, e];
+    array.sort();
+    println!("{}", (n + array[0] - 1) / array[0] + 4);
 }
